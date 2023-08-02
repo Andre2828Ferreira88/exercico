4f2243fa543d8692f626933e66cd7e244e171af6
Andre2828Ferreira88 <123772724+Andre2828Ferreira88@users.noreply.github.com>
2023-08-02 10:57:34 -0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,34 @@
+var favoriteFood = "pizza"
+console.log("a melhor " + favoriteFood +" de osasco")
+
+var numOfSlices = 8
+console.log(" a pizza tem " + numOfSlices + " pedaços")
+
+let changeMe = true
+console.log(changeMe)
+
+changeMe = false
+console.log(changeMe)
+
+const entree = 'Enchiladas'
+console.log(entree)
+
+var levelUp = 5
+console.log(++ levelUp)
+
+
+var multiplyMe = 5
+var multiplico = 11
+console.log(multiplico * multiplyMe)
+
+var quarterMe = 13
+var dividendo = 4
+console.log(multiplico / multiplyMe)
+
+
+var nome = "André"
+var city = "Osasco"
+console.log("meu nome é " + nome + " e eu moro em " + city)
+
+var eu = "andré"
+console.log( typeof eu);
